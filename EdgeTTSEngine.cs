b08b07dbd1101c139352e525575702c19203d86b
AtmoOmen <atmoomen2004@outlook.com>
2024-11-16 17:35:17 +0800

    Update Voices

--- EdgeTTSEngine.cs
+++ EdgeTTSEngine.cs
@@ -23,13 +23,24 @@ public sealed class EdgeTTSEngine(string cacheFolder) : IDisposable
         new("zh-CN-YunyangNeural", "云扬 (中文-普通话-新闻-男)"),
         new("zh-CN-YunxiaNeural", "云霞 (中文-普通话-儿童-男)"),
         new("zh-CN-YunxiNeural", "云希 (中文-普通话-男)"),
-        new("zh-HK-HiuMaanNeural", "曉佳 (中文-廣東話-女)"),
+        new("zh-HK-HiuGaaiNeural", "曉佳 (中文-廣東話-女)"),
+        new("zh-HK-HiuMaanNeural", "曉曼 (中文-廣東話-女)"),
+        new("zh-HK-WanLungNeural", "雲龍 (中文-廣東話-男)"),
         new("zh-TW-HsiaoChenNeural", "曉臻 (中文-國語-女)"),
+        new("zh-TW-HsiaoYuNeural", "曉雨 (中文-國語-女)"),
+        new("zh-TW-YunJheNeural", "雲哲 (中文-國語-男)"),
         new("ja-JP-NanamiNeural", "七海 (日本語-女)"),
+        new("ja-JP-KeitaNeural", "庆太 (日本語-男)"),
         new("en-US-AriaNeural", "Aria (English-American-Female)"),
         new("en-US-JennyNeural", "Jenny (English-American-Female)"),
+        new("en-US-AnaNeural", "Ana (English-American-Child-Female)"),
+        new("en-US-MichelleNeural", "Michelle (English-American-Female)"),
         new("en-US-GuyNeural", "Guy (English-American-Male)"),
-        new("en-GB-SoniaNeural", "Sonia (English-Britain-Female)")
+        new("en-US-ChristopherNeural", "Christopher (English-American-Male)"),
+        new("en-US-EricNeural", "Eric (English-American-Male)"),
+        new("en-US-RogerNeural", "Roger (English-American-Male)"),
+        new("en-US-SteffanNeural", "Steffan (English-American-Male)"),
+        new("en-GB-SoniaNeural", "Sonia (English-Britain-Female)"),
     ];
 
     public void Speak(string text, EdgeTTSSettings settings)
@@ -45,7 +56,7 @@ public sealed class EdgeTTSEngine(string cacheFolder) : IDisposable
     public async Task SpeakAsync(string text, EdgeTTSSettings settings)
     {
         ThrowIfDisposed();
-        var safeText = SecurityElement.Escape(text);
+        var safeText = SecurityElement.Escape(text).Replace('：', ':');
         var audioFile = await GetOrCreateAudioFileAsync(safeText, settings);
 
         if (!string.IsNullOrWhiteSpace(audioFile))
